de415e01ae98ae4743f58ddf28461999d9778c21
nmhung2022 <nmhung2022@gmail.com>
2021-10-19 14:23:54 +0700

    implement drag and drop, eslint

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -2,23 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Column.scss';
 import Card from 'components/Card/Card';
-import { mapOder } from 'utilities/sorts';
+import { Container, Draggable } from 'react-smooth-dnd';
+import { mapOrder } from 'utilities/sorts';
 
 Column.propTypes = {};
 
 function Column(props) {
   const { column } = props;
-  const cards = column.cards
-  // mapOder(column.cards, column.cardOrder, 'id');
+  const cards = mapOrder(column.cards, column.cardOrder, 'id');
+
+  const onCardDrop = () => {};
   return (
     <div className="column">
-      <header>{column.title}</header>
+      <header className="column-drag-handle">{column.title}</header>
 
-      <ul className="card-list">
-        {cards.map((card, index) => (
-          <Card key={index} card={card} />
-        ))}
-      </ul>
+      <div className="card-list">
+        <Container
+          orientation="vertical"
+          groupName="col"
+          onDrop={onCardDrop}
+          getChildPayload={(index) => cards[index]}
+          dragClass="card-ghost"
+          dropClass="card-ghost-drop"
+          dropPlaceholder={{
+            animationDuration: 150,
+            showOnTop: true,
+            className: 'card-drop-preview',
+          }}
+          dropPlaceholderAnimationDuration={200}
+        >
+          {cards.map((card, index) => (
+            <Draggable key={index}>
+              <Card card={card} />
+            </Draggable>
+          ))}
+        </Container>
+      </div>
       <footer>Add another card</footer>
     </div>
   );
