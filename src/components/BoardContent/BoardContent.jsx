d67590da745b0c83d4e71acac5b41f89cf050a7b
nmhung2022 <nmhung2022@gmail.com>
2021-10-19 18:20:02 +0700

    handle state change when drag and drop

--- src/components/BoardContent/BoardContent.jsx
+++ src/components/BoardContent/BoardContent.jsx
@@ -6,6 +6,7 @@ import { initialData } from 'actions/initialData';
 import { isEmpty } from 'lodash';
 import { Container, Draggable } from 'react-smooth-dnd';
 import { mapOrder } from 'utilities/sorts';
+import { applyDrag } from 'utilities/dragDrop';
 
 BoardContent.propTypes = {};
 
@@ -27,7 +28,26 @@ function BoardContent(props) {
   }
 
   const onColumnDrop = (dropResult) => {
-    console.log(dropResult);
+    let newColumns = [...columns];
+    newColumns = applyDrag(newColumns, dropResult);
+
+    let newBoard = { ...board };
+    newBoard.columnOrder = newColumns.map((c) => c.id);
+    newBoard.columns = newColumns;
+
+    setColumns(newColumns);
+    setBoard(newBoard);
+  };
+
+  const onCardDrop = (columnId, dropResult) => {
+    if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
+      let newColumns = [...columns];
+
+      let currentColumn = newColumns.find((c) => c.id === columnId);
+      currentColumn.cards = applyDrag(currentColumn.cards, dropResult);
+      currentColumn.cardOrder = currentColumn.cards.map((i) => i.id);
+      setColumns(newColumns);
+    }
   };
 
   return (
@@ -45,9 +65,13 @@ function BoardContent(props) {
       >
         {columns.map((column, index) => (
           <Draggable key={index}>
-            <Column column={column} />
+            <Column column={column} onCardDrop={onCardDrop} />
           </Draggable>
         ))}
+        <div className="add-new-column">
+          <i className="fa fa-plus icon" />
+          Add another card
+        </div>
       </Container>
     </div>
   );
