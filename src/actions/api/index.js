bcfeaa6d73a6ccaf8f5bef65d0ee1bdf07e58699
nmhung2022 <nmhung2022@gmail.com>
2021-10-25 21:51:42 +0700

    CRUD column & card

--- src/actions/api/index.js
+++ src/actions/api/index.js
@@ -2,6 +2,24 @@ import axios from 'axios';
 import { API_ENDPOINT } from 'utilities/constants';
 
 export const fetchBoardDetail = async (id) => {
-  const { data } = await axios.get(`${API_ENDPOINT}/api/v1/boards/${id}`);
-  return data;
+  const response = await axios.get(`${API_ENDPOINT}/api/v1/boards/${id}`);
+  return response.data;
+};
+
+export const createNewColumn = async (data) => {
+  const response = await axios.post(`${API_ENDPOINT}/api/v1/columns`, data);
+  return response.data;
+};
+// Update or remove column
+export const updateColumn = async (id, data) => {
+  const response = await axios.put(
+    `${API_ENDPOINT}/api/v1/columns/${id}`,
+    data
+  );
+  return response.data;
+};
+
+export const createNewCard = async (data) => {
+  const response = await axios.post(`${API_ENDPOINT}/api/v1/cards`, data);
+  return response.data;
 };
